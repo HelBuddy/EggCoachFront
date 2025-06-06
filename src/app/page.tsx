99223b64abafab03bce9fd1e4ac2903fee407c05
godboy4256 <godboy4256@naver.com>
2025-06-06 19:24:31 +0900

    globals.css : 공용 스타일 CSS 세팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,7 @@ const Home = () => {
   return (
     <div className='flex h-screen items-center justify-center bg-white'>
       <div>
-        <span>TEST PAGE</span>
-
-        {/* svg 사용 example */}
+        <h1 className='bg-main-2'>메인 컬러 테스트</h1>
         <TestSvg />
       </div>
     </div>
--- src/widgets/header/ui/organisms/HeaderContainer.tsx
+++ src/widgets/header/ui/organisms/HeaderContainer.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const HeaderContainer = () => {
-  return <div>HeaderContainer</div>
+  return <div></div>
 }
 
 export default HeaderContainer
